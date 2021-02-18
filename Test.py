626d0bfe070938a1d2036794ac1816057eb6199e
baskakovanton <8Bazz8@mail.ru>
2021-02-18 22:20:10 +1000

    Lesson 1

---
+++ Test.py
@@ -0,0 +1,8 @@
+duration = int(input('Введите данные: '))
+
+if duration == 0 or duration >= 5:
+    print(duration, 'процентов')
+elif duration == 1:
+    print(duration, 'процент')
+else:
+    print(duration, 'процента')
