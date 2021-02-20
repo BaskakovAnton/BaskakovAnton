0fa55b61701b0d388675ace0a051cd705609430f
baskakovanton <8Bazz8@mail.ru>
2021-02-20 18:29:15 +1000

    edit Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -5,35 +5,26 @@ for i in range(1, 1000):
     else:
         list_numbers.append(i ** 3)
 
-idx = 0
-sum_numbers = 0
+result_a = 0
+result_b = 0
 
-while idx < len(list_numbers):
-    number = list_numbers[idx]
-    str_number = str(number)
-    str_number = str_number.replace('.', '')
-    lst_str = list(str_number)
-    lst_number = map(int, lst_str)
-    s = sum(lst_number)
-    if s % 7 == 0:
-        sum_numbers = sum_numbers + s
-    idx += 1
+for idx in range(len(list_numbers)):
+    num = list_numbers[idx]
+    digit_sum = 0
+    while num > 0:
+        digit_sum += num % 10
+        num //= 10
+    if digit_sum % 7 == 0:
+        result_a += list_numbers[idx]
 
-idx_1 = 0
-sum_numbers_1 = 0
+for idx in range(len(list_numbers)):
+    num = list_numbers[idx] + 17
+    digit_sum = 0
+    while num > 0:
+        digit_sum += num % 10
+        num //= 10
+    if digit_sum % 7 == 0:
+        result_b += list_numbers[idx]
 
-while idx_1 < len(list_numbers):
-    number_1 = list_numbers[idx_1]
-    s_2 = list_numbers[idx_1] + 17
-    str_number = str(s_2)
-    str_number = str_number.replace('.', '')
-    lst_str = list(str_number)
-    lst_number = map(int, lst_str)
-    s_1 = sum(lst_number)
-    if s_1 % 7 == 0:
-        sum_numbers_1 = sum_numbers_1 + s_1
-    idx_1 += 1
-
-print('Cписок кубов: ', list_numbers)
-print('Сумма №1: ', sum_numbers)
-print('Сумма №2: ', sum_numbers_1)
+print('Сумма чисел №1: ', result_a, 'Правильный ответ: 17485588610')
+print('Сумма чисел №2: ', result_b, 'Правильный ответ: 15392909930')
