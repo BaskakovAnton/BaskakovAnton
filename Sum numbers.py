d839d73bdcf481f15f5ac4b624dc46fedc47059d
baskakovanton <8Bazz8@mail.ru>
2021-02-20 18:28:07 +1000

    edit Task_2.py

--- Sum numbers.py
+++ Sum numbers.py
@@ -3,7 +3,7 @@ for i in range(1, 1000):
     if i % 2 != 1:
         pass
     else:
-        list_numbers.append(i**3)
+        list_numbers.append(i ** 3)
 
 result_a = 0
 result_b = 0
@@ -11,7 +11,7 @@ result_b = 0
 for idx in range(len(list_numbers)):
     num = list_numbers[idx]
     digit_sum = 0
-    while num != 0:
+    while num > 0:
         digit_sum += num % 10
         num //= 10
     if digit_sum % 7 == 0:
@@ -20,12 +20,11 @@ for idx in range(len(list_numbers)):
 for idx in range(len(list_numbers)):
     num = list_numbers[idx] + 17
     digit_sum = 0
-    while num != 0:
+    while num > 0:
         digit_sum += num % 10
         num //= 10
     if digit_sum % 7 == 0:
         result_b += list_numbers[idx]
 
-
-print(result_a)
-print(result_b)
+print('Сумма чисел №1: ', result_a, 'Правильный ответ: 17485588610')
+print('Сумма чисел №2: ', result_b, 'Правильный ответ: 15392909930')
