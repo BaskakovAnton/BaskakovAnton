35302a94cc1852de1b9fc64ad0e4a961eed82cb1
baskakovanton <8Bazz8@mail.ru>
2021-02-22 19:38:09 +1000

    edit Task_3_True

--- Task_3_True.py
+++ Task_3_True.py
@@ -1,9 +1,9 @@
-#percent = int(input('Введите данные: '))
+percent = int(input('Введите данные: '))
 
-for percent in range(32):
-    if percent % 10 == 1 and percent % 100 != 11:
-        print(percent, 'процент', end=" ")
-    elif 1 < percent % 10 < 5 and not 11 < percent % 100 < 15:
-        print(percent, 'процента', end=" ")
-    else:
-        print(percent, 'процентов', end=" ")
+# for percent in range(32):
+if percent % 10 == 1 and percent % 100 != 11:
+    print(percent, 'процент', end=" ")
+elif 1 < percent % 10 < 5 and not 11 < percent % 100 < 15:
+    print(percent, 'процента', end=" ")
+else:
+    print(percent, 'процентов', end=" ")
