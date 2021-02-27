a17dfb9c9ab42c72dce0fb4a903c809fda131aed
BaskakovAnton <8Bazz8@mail.ru>
2021-02-28 00:51:56 +1000

    edit Tasks_2

--- Task_2.py
+++ Task_2.py
@@ -12,16 +12,3 @@ for i in starting_list:
 
 print(correct_list)
 print(" ".join(correct_list))
-
-
-starting_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
-
-for i, v in enumerate(starting_list):
-    if v.replace("+", "").replace("-", "").isdigit():
-        if v.isdigit():
-            starting_list[i] = f'"{int(v):02}"'
-        else:
-            starting_list[i] = f'"{v[0]}{int(v[1:]):02}"'
-
-print(starting_list)
-print(" ".join(starting_list))
