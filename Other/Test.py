ce13ba0ae546ebd6ef6072d91bd3d47efa956857
BaskakovAnton <8Bazz8@mail.ru>
2021-02-28 18:53:21 +1000

    edit Test.py

--- Other/Test.py
+++ Other/Test.py
@@ -1,54 +1,49 @@
-# Создать список, состоящий из кубов нечётных чисел от 1 до 1000:
-# a. Вычислить сумму тех чисел из этого списка, сумма цифр которых делится нацело на 7.
-# Например, число «19 ^ 3 = 6859» будем включать в сумму, так как 6 + 8 + 5 + 9 = 28 –
-# делится нацело на 7. Внимание: использовать только арифметические операции!
-# b. К каждому элементу списка добавить 17 и заново вычислить сумму тех чисел из этого
-# списка, сумма цифр которых делится нацело на 7.
-# c. * Решить задачу под пунктом b , не создавая новый список.
-
-list_numbers = []
-for i in range(1, 1000):
-    if i % 2 != 1:
-        pass
-    else:
-        list_numbers.append(i ** 3)
+# def my_f(s_1, s_2):
+# sub = s_1 - s_2
+# print(f"Sub: {sub}")
 
 
-idx = 0
-list_numbers_1 = []
-sum_numbers = 0
+# my_f(s_2=23, s_1=67)
+# my_f(int(input()), int(input()))
 
-while idx < len(list_numbers):
-    number = list_numbers[idx]
-    str_number = str(number)
-    str_number = str_number.replace('.', '')
-    lst_str = list(str_number)
-    lst_number = map(int, lst_str)
-    s = sum(lst_number)
-    if s % 7 == 0:
-        sum_numbers = sum_numbers + list_numbers[idx]
-    list_numbers_1.append(s)
-    idx += 1
-
-idx_1 = 0
-sum_numbers_1 = 0
-list_numbers_2 = []
-while idx_1 < len(list_numbers):
-    number_1 = list_numbers[idx_1]
-    s_2 = list_numbers[idx_1] + 17
-    str_number = str(s_2)
-    str_number = str_number.replace('.', '')
-    lst_str = list(str_number)
-    lst_number = map(int, lst_str)
-    s_1 = sum(lst_number)
-    if s_1 % 7 == 0:
-        sum_numbers_1 = sum_numbers_1 + s_1
-    list_numbers_2.append(s_1)
-    idx_1 += 1
+# ____________________________________________________________________
+# def my_f1(*args):
+# print("Hi")
+# return args
 
 
+# print(my_f1(23, 68, 45, False))
 
-#print(list_numbers_1)
-print(sum_numbers)
-#print(list_numbers_2)
-print(sum_numbers_1)
+# ____________________________________________________________________
+# def my_f1(**kwargs):
+# return kwargs
+
+
+# print(my_f1(name=23, age=68, surname=45, s=False))
+
+# ____________________________________________________________________
+# my_f = lambda s_1, s_2: s_1 - s_2
+# print(my_f(78, 45))
+
+# ____________________________________________________________________
+# m = 78
+
+
+# def my_f():
+# global n
+# n = 7
+# return n + m
+
+
+# print(my_f())
+# print(n)
+# ___________________________________________________________________
+
+def accepted(el):
+    return el.lower().startswith('i')
+
+
+products = ['IPad', 'Samsung Galaxy', 'iPhone', 'iRiver']
+products_sample = filter(accepted, products)
+print(type(products_sample))
+print(*products_sample)
