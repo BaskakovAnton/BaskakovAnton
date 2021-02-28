c1d9e7b94798cb8ce534f8b154ceda5b0054eb2b
BaskakovAnton <8Bazz8@mail.ru>
2021-03-01 00:33:21 +1000

    edit Test.py

--- Other/Test.py
+++ Other/Test.py
@@ -39,11 +39,20 @@
 # print(n)
 # ___________________________________________________________________
 
-def accepted(el):
-    return el.lower().startswith('i')
+#def accepted(el):
+    #return el.lower().startswith('i')
 
 
-products = ['IPad', 'Samsung Galaxy', 'iPhone', 'iRiver']
-products_sample = filter(accepted, products)
-print(type(products_sample))
-print(*products_sample)
+#products = ['IPad', 'Samsung Galaxy', 'iPhone', 'iRiver']
+#products_sample = filter(accepted, products)
+#print(type(products_sample))
+#print(*products_sample)
+
+
+day_sales = [1589.5, 2687.5, 5478.2, 1236.5, 4756.5]
+day_sales1 =[]
+for idx, item in enumerate(day_sales, start=1):
+    day_sales1.append(idx item)
+    print('товар №', idx, '-', item)
+
+print(day_sales1)
