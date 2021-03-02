11e3cb916ed4ae7cccd72f75513ff77a7c9293bd
BaskakovAnton <8Bazz8@mail.ru>
2021-03-02 15:36:44 +1000

    edit Tasks

---
+++ Task_1.py
@@ -0,0 +1,23 @@
+def num_translate():
+    for i in dic_word:
+        if userword == i:
+            return (dic_word.get(i))
+
+dic_word = {
+    'zero': 'ноль',
+    'one': 'один',
+    'two': 'два',
+    'three': 'три',
+    'four': 'четыре',
+    'five': 'пять',
+    'six': 'шесть',
+    'seven': 'семь',
+    'eight': 'восемь',
+    'nine': 'девять',
+    'ten': 'десять',
+}
+
+userword = input('Введите значение: ')
+answer = num_translate()
+print(answer)
+
