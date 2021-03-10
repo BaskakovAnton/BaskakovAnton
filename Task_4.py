3999ec83f8d907bb30fc812b61587fc5689e8964
baskakovanton <8Bazz8@mail.ru>
2021-03-11 00:10:33 +1000

    edit Tasks

---
+++ Task_4.py
@@ -0,0 +1,13 @@
+src = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+# [12, 44, 4, 10, 78, 123]
+
+#result = [i for i in range(len(src))]
+
+result = []
+
+for i, j in range(len(src)):
+    if i > 2:
+        result.append()
+
+
+print(type(result), result)
