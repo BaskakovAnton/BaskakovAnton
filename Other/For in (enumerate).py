fbbf26615453f9e0cbe38674ff15bb6c63e6f7bc
baskakovanton <8Bazz8@mail.ru>
2021-03-14 01:24:04 +1000

    delete files

--- Other/For in (enumerate).py
+++
@@ -1,4 +0,0 @@
-day_sales = [1589.5, 2687.5, 5478.2, 1236.5, 4756.5]
-
-for idx, item in enumerate(day_sales, start=1):
-    print('товар №', idx, '-', item)
--- Other/For in (index).py
+++
@@ -1,9 +0,0 @@
-day_sales = [1589.5, 2687.5, 5478.2, 1236.5, 4756.5]
-
-total_sales = 0
-
-for product_price in day_sales:
-    total_sales += product_price
-
-price_per_product = total_sales / len(day_sales)
-print(price_per_product)
