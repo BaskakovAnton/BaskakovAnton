f02a0b227f47e788af6faac1c4d0c447fd8413c3
baskakovanton <8Bazz8@mail.ru>
2021-03-16 21:48:41 +1000

    edit tasks

--- Task_2.py
+++ Task_2.py
@@ -4,5 +4,6 @@ from collections import Counter
 with open("nginx_logs.txt", "r", encoding="utf-8") as parsing:
     content = ((line.split()[0]) for line in parsing)
     content = Counter(content)
-    spamer = max(content.items(), key=operator.itemgetter(1))
-    print("Спамер: ", spamer)
+    spammer = max(content.items(), key=operator.itemgetter(1))[0]
+    spammer_request = max(content.items(), key=operator.itemgetter(1))[1]
+    print("Спамер: ", spammer, "Колличество запросов: ", spammer_request)
