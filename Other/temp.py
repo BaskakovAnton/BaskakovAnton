338bfb34c149447086db1bd4018727dd70a7ac23
baskakovanton <8Bazz8@mail.ru>
2021-03-21 20:35:32 +1000

    edit temp.py

--- Other/temp.py
+++ Other/temp.py
@@ -1,19 +1,26 @@
-x = float(input("Введите значение x ="))
-y = float(input("Введите значение y ="))
-z = input("Введите оператор (+, -, /, *, mod, pow, div) =")
-if z == '+':
-    result = x + y
-elif z == '-':
-    result = x - y
-elif z == pow:
-    result = pow(x, y)
-elif z == '*':
-    result = x * y
-elif y != 0:
-    if z == '/':
-        result = x / y
-elif z == 'div':
-    result = x // y
-elif z == 'mod':
-    result = x % y
-print("Результат вычислений =", result)
+a = float(input())
+b = float(input())
+c = input()
+if c == '+':
+    print(a + b)
+elif c == '-':
+    print(a - b)
+elif c == '/':
+    if b == 0:
+        print("Деление на 0!")
+    else:
+        print(a / b)
+elif c == '*':
+    print(a * b)
+elif c == 'mod':
+    if b == 0:
+        print("Деление на 0!")
+    else:
+        print(a % b)
+elif c == 'pow':
+    print(a ** b)
+elif c == 'div':
+    if b == 0:
+        print("Деление на 0!")
+    else:
+        print(a // b)
