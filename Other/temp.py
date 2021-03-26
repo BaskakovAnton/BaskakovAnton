7fe3b032f2e51d7437d75b75c26967f1d8ff4a85
baskakovanton <8Bazz8@mail.ru>
2021-03-27 09:14:29 +1000

    edit

--- Other/temp.py
+++ Other/temp.py
@@ -1,15 +1,31 @@
-# a = int(input())
-# b = int(input())
-# c = int(input())
-# d = int(input())
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+for h in range(c, d + 1):
+    print('\t' + str(h), end='')
+print(end='\n')
+for i in range(a, b + 1):
+    print(str(i) + '\t', end='')
+    for j in range(c, d + 1):
+        print(str(i * j), end='\t')
+    print(end='\n')
 
 
+
+
+# #Ввод данных
 # a = 7
 # b = 10
 # c = 5
 # d = 6
-
-for j in range(5, 6 + 1):
-    print(j)
-    for i in range(7, 10 + 1):
-        print(i, i * j, end='\n')
+# #начало Цикла
+# for g in range (c,d+1):
+#     print('\t'+str(g),end='')
+# print(end='\n')
+# for i in range (a,b+1):
+#     print(str(i)+'\t',end='')
+#     for j in range (c,d+1):
+#         print(str(i * j),end='\t')# выводим результат умножения соответствующих чисел в таблицу
+#     print(end='\n')
